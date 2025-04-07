b51f8afc5ce88e1922ac53527509def1f4df4eeb
BIANKA MOURA DE MATOS <bianka.mmatos@senacsp.edu.br>
2025-04-07 19:47:13 -0300

    prod

--- js/script.js
+++ js/script.js
@@ -1,76 +1,81 @@
+//  Selecionando os elementos do meu mobile
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+ 
+// Fechar o menu
+const about = document.querySelector('#mobile-navbar [href="#about"]');
+const header = document.querySelector('#mobile-navbar [href="#header"]');
+const expertiseareas = document.querySelector(
+  '#mobile-navbar [href="#expertise-areas"]'
+);
+const team = document.querySelector('#mobile-navbar [href="#team"]');
+const contact = document.querySelector('#mobile-navbar [href="#contact"]');
+ 
 // selecionar os elementos dos slides
 const slides = document.querySelectorAll(".banner");
 //
 const dots = document.querySelectorAll(".dot");
 // variavel de controle
-let slideIndex = 0;
+let slideIndex = 1;
  
-function showSlides(){
-    for(let i=0; i < slides.length; i++){
-        slides[i].classList.remove("active")
-        dots[i].classList.remove("active")
-    }
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    dots[i].classList.remove("active");
+  }
  
-    slideIndex++; // avançando a variavel
-    if (slideIndex > slides.length){
-        slideIndex = 1;
-    }
-    console.log(slideIndex)
-    console.log(slides[slideIndex -1])
-    slides[slideIndex -1].classList.add("active")
-    dots[slideIndex -1].classList.add("active")
+  slideIndex++; // avançando a variavel
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  console.log(slideIndex);
+  console.log(slides[slideIndex - 1]);
+  slides[slideIndex - 1].classList.add("active");
+  dots[slideIndex - 1].classList.add("active");
  
-    setTimeout(showSlides, 3000)
- 
+  setTimeout(showSlides, 3000);
 }
  
+// ativar menu-mobile
+ 
+[menuBtn, closeMenuBtn].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    menu.classList.toggle("menu-active");
+  });
+});
+ 
+// menuBtn.addEventListener("click", (e) => {
+//   menu.classList.add("menu-active");
+// });
+ 
+// closeMenuBtn.addEventListener("click", (e) => {
+//   menu.classList.remove("menu-active");
+// });
+ 
+// fechar o menu
+about.addEventListener("click", (e) => {
+  menu.classList.remove("menu-active");
+});
+ 
+header.addEventListener("click", (e) => {
+  menu.classList.remove("menu-active");
+});
+ 
+expertiseareas.addEventListener("click", (e) => {
+  menu.classList.remove("menu-active");
+});
+ 
+team.addEventListener("click", (e) => {
+  menu.classList.remove("menu-active");
+});
+ 
+contact.addEventListener("click", (e) => {
+  menu.classList.remove("menu-active");
+});
+ 
 //Inicialização
-showSlides()
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+showSlides();
  
 // DOM
 // Navegar no documento html
@@ -81,4 +86,5 @@ showSlides()
 //     // console.log(event.target)
 //     event.target.style.color ="blue";
 // })
+ 
  
